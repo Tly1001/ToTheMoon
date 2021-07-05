0c9cdf5fd032ca2ed76c3b262961dfd7ad1e078e
tly1001 <thomasly1992@gmail.com>
2021-07-05 20:39:44 +0100

    added put, changed get for single user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const User = require('../models/user.model')
 
-// GET all users (need to change to specific user)
-router.get('/', (req, res) => {
-  User.find()
+// GET user data 
+//TODO change to feed back specific info, not sure what info yet
+router.get('/:id', (req, res) => {
+  User.findById(req.params.id)
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err))
 })
@@ -22,5 +23,28 @@ router.post('/register', (req, res) => {
     .then(() => res.json('User registered!'))
     .catch(err => res.status(400).json('Error: ' + err))
 })
+
+// PUT update User
+//TODO need to look into the schema again, maybe move bookmarks, cash and portfolio into one property?
+router.put('/update/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then( user => {
+      user.firstName = req.body.firstName
+      user.lastName = req.body.lastName
+      user.password = req.body.password
+      user.birthday = req.body.birthday
+      user.bookmarks = req.body.bookmarks
+      user.cash = req.body.cash
+      user.portfolio = req.body.portfolio
+
+      user.save()
+        .then(() => res.json('User updated!'))
+        .catch(err => res.status(400).json('Error: ' + err))
+    })
+    .catch( err => res.status(400).json('Error: ' + err))
+})
+
+// Delete User?
+
 
 module.exports = router

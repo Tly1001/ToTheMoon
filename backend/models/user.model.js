d6576ca1e893e728163f2b486d901e28db1412e0
tly1001 <thomasly1992@gmail.com>
2021-07-06 15:14:46 +0100

    added new field, added validation, finished portfolio field

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const validateEmail = (email) => {
+  const re = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+  return re.test(email)
+}
+
 const userSchema = new Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: 'Valid email address is required',
+    validate: [validateEmail, 'Please use a valid email address'],
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+  },
   firstName: {
     type: String,
     required: true,
@@ -18,13 +32,20 @@ const userSchema = new Schema({
     minlength: 1
   },
   bookmarks: [String],
-  cash: Number,
   portfolio: {
-    aggregates: {},
-    purchaseHistory: {}
+    wallet: {
+      currencyCode: { type: String, default: 'gbp' },
+      amount: { type: Number, default: 10000 }
+    },
+    transactions: {
+      currencyCode: { type: String },
+      amount: { type: Number },
+      date: { type: Date }
+    }
   }
 }, {
   timestamps: true
+  // createdAt and updatedAt
 })
 
 const User = mongoose.model('User', userSchema)

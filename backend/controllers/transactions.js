d2a7488840552ea7128ec37c6f335f2bb2045cbe
tly1001 <thomasly1992@gmail.com>
2021-07-07 22:24:22 +0100

    added input of transaction history

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -10,14 +10,26 @@ async function purchase(req, res) {
 
     const user = await User.findById(req.params.id)
     const newUser = JSON.parse(JSON.stringify(user))
+
+    // wallet input
     const wallet = newUser.portfolio.wallet
     if (buyAmount > wallet.gbp) throw new Error()
-
     // remove money
     wallet.gbp -= buyAmount 
     // add crypto
     wallet[buyCode] = (wallet[buyCode] ? wallet[buyCode] + buyAmount : buyAmount)
 
+    // transaction history
+    const transactions = newUser.portfolio.transactions
+    const date = new Date()
+    const newEntry = {
+      currencyCode: buyCode,
+      amount: buyAmount,
+      date: date
+    }
+    transactions.push(newEntry)
+
+    // save to db
     Object.assign(user, newUser)
     await user.save()
     res.status(202).json(user)

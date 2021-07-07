2f0ce7e1d392b4c1e67104750da4d2d0624e4d67
tly1001 <thomasly1992@gmail.com>
2021-07-07 22:50:22 +0100

    added sell function

--- backend/controllers/transactions.js
+++ backend/controllers/transactions.js
@@ -2,6 +2,8 @@ const User = require('../models/user.model')
 
 // POST purchase
 //TODO need to change to find user with token
+//? maybe put into one function? very small variance
+//? also maybe add cash value to transation history?
 async function purchase(req, res) {
   try {
     const buyCode = 'btc'
@@ -10,7 +12,7 @@ async function purchase(req, res) {
     const user = await User.findById(req.params.id)
     const newUser = JSON.parse(JSON.stringify(user))
 
-    // wallet input
+    // wallet changes
     const wallet = newUser.portfolio.wallet
     if (buyAmount > wallet.gbp) throw new Error()
     // remove money
@@ -20,12 +22,7 @@ async function purchase(req, res) {
 
     // transaction history
     const transactions = newUser.portfolio.transactions
-    const date = new Date()
-    const newEntry = {
-      currencyCode: buyCode,
-      amount: buyAmount,
-      date: date
-    }
+    const newEntry = createNewEntry(buyCode, buyAmount, true)
     transactions.push(newEntry)
 
     // save to db
@@ -39,6 +36,51 @@ async function purchase(req, res) {
   }
 }
 
+// POST sell
+async function sell(req, res) {
+  try {
+    const sellCode = 'btc'
+    const sellAmount = req.body.sellAmount
+
+    const user = await User.findById(req.params.id)
+    const newUser = JSON.parse(JSON.stringify(user))
+
+    // wallet changes
+    const wallet = newUser.portfolio.wallet
+    if ( !wallet[sellCode] || sellAmount > wallet[sellCode]) throw new Error()
+    // add money
+    wallet.gbp += sellAmount 
+    // remove crypto
+    wallet[sellCode] -= sellAmount
+
+    // transaction history
+    const transactions = newUser.portfolio.transactions
+    const newEntry = createNewEntry(sellCode, sellAmount, false)
+    transactions.push(newEntry)
+        
+    // save to db
+    Object.assign(user, newUser)
+    await user.save()
+    res.status(202).json(user)
+  } catch (err) {
+    res.status(402).json({
+      'Message': 'Unable to process request ' + err
+    })
+  }
+}
+
+function createNewEntry(code, amount, isBuying) {
+  const date = new Date()
+  const newEntry = {
+    currencyCode: code,
+    type: isBuying ? 'buy' : 'sell',
+    amount: amount,
+    date: date
+  }
+  return newEntry
+}
+
 module.exports = {
-  purchase
+  purchase,
+  sell
 }

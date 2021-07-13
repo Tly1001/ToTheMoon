d38f72809aaa0f40ca15174f50676603b958f456
tly1001 <thomasly1992@gmail.com>
2021-07-13 20:16:46 +0100

    made routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h1>Hello World</h1>
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './components/common/Home'
+import Navbar from './components/common/Navbar'
+import Register from './components/auth/Register'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/register" component={Register}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App

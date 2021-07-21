e9070afe3281276b58c6db44503812d5d59c250e
tly1001 <thomasly1992@gmail.com>
2021-07-21 17:58:51 +0100

    converted some css to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        '5px': '5px',
+      },
+      height: {
+        '4px': '4px',
+      },
+      width: {
+        '33px': '33px',
+      },
+    },
     backgroundColor: theme => ({
       ...theme('colors'),
       'primaryBlue': '#191970',

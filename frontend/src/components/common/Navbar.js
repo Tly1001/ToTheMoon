678cc2d70a4d8cf0ee28a1134072015ec973d787
tly1001 <thomasly1992@gmail.com>
2021-07-21 14:51:16 +0100

    added links moved styles to a variable

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,29 +1,14 @@
 import React from 'react'
-// import { Link, withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  // const classes = useStyles()
+  const [dropdown, setDropdown] = React.useState('false')
 
-  // <div className={classes.root}>
-  //   <AppBar position="static">
-  //     <Toolbar>
-  //       <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-  //         <MenuIcon />
-  //       </IconButton>
-  //       <Typography variant="h6" className={classes.title}>
-  //         <Link className="nav-item">To The Moon</Link>
-  //       </Typography>
-
-  //       <Link className="nav-item nav-button">LOGIN</Link>
-  //       <Link to="/register" className="nav-item nav-button">REGISTER</Link>
-
-  //     </Toolbar>
-  //   </AppBar>
-  // </div>
+  const sidebarItemStyle = 'block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-white focus:text-gray-900 hover:bg-primaryGrey focus:bg-gray-200 focus:outline-none focus:shadow-outline'
 
   return ( <>
-    <div className="md:flex flex-col md:flex-row md:min-h-screen w-full absolute">
-      <div  className="flex flex-col w-full md:w-64 text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0" x-data="{ open: false }">
+    <div className="md:flex flex-col md:flex-row md:min-h-screen hidden w-full absolute">
+      <div  className="flex flex-col w-full md:w-64 text-gray-700 bg-primaryWhite dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0" x-data="{ open: false }">
         <div className="flex-shrink-0 px-8 py-4 flex flex-row items-center justify-between">
           <a href="#" className="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">To The Moon</a>
           <button className="rounded-lg md:hidden rounded-lg focus:outline-none focus:shadow-outline">
@@ -34,24 +19,28 @@ const Navbar = () => {
           </button>
         </div>
         <nav className="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
-          {/* <a className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Home</a> */}
-          <a className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Home</a>
-          <a className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Portfolio</a>
-          <a className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Login</a>
-          <a className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Register</a>
-          <a className="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Contact</a>
-          <div className="relative" x-data="{ open: false }">
-            <button  className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
+          <Link to="/" className={sidebarItemStyle} >Home</Link>
+          <Link to="/portfoilio" className={sidebarItemStyle} href="#">Portfolio</Link>
+          <Link to="/login" className={sidebarItemStyle} href="#">Login</Link>
+          <Link to="/register" className={sidebarItemStyle} href="#">Register</Link>
+          <Link to="/contact" className={sidebarItemStyle} href="#">Contact</Link>
+          <div className="relative" 
+          // x-data="{ open: false }"
+            onClick={() => 
+              // console.log('dropdown will be ', !dropdown)
+              setDropdown(!dropdown)
+            }>
+            <button className="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
               <span>Dropdown</span>
               <svg fill="currentColor" viewBox="0 0 20 20"  className="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
                 <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path>
               </svg>
             </button>
-            <div x-show="open" className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
+            <div x-show={String(dropdown)} className="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
               <div className="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-                <a className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #1</a>
-                <a className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #2</a>
-                <a className="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>
+                <a className={sidebarItemStyle} href="#">Link #1</a>
+                <a className={sidebarItemStyle} href="#">Link #2</a>
+                <a className={sidebarItemStyle} href="#">Link #3</a>
               </div>
             </div>
           </div>
